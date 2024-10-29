340f5a3ec3b78c0ef43ca8597dba0dd58240d39e
Busyadex <Bussyadex42@gmail.com>
2024-10-29 00:25:03 +0000

    AzureCommit

---
+++ AzureADWeb/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+}).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+	.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+	{
+		options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+		options.Authority = "";
+		options.ClientId = "";
+		options.ResponseType = "code";
+		options.SaveTokens = true;
+		options.Scope.Add("");
+		options.ClientSecret = "";
+	});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
